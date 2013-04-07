630a27fe864e3babb0cefbfc80e2a953ffe78518
Nick Heiner <nick.heiner@opower.com>
2013-04-07 12:52:13 -0400

    adding service stubs

---
+++ app/scripts/services/legalMoves.js
@@ -0,0 +1,6 @@
+angular.module('ChessScouterApp')
+    .factory('getLegalMoves', function() {
+        return function(game, x, y) {
+            // return a list of legal moves for the piece located at (x, y) in the game
+        }
+    });
---
+++ app/scripts/services/pins.js
@@ -0,0 +1,6 @@
+angular.module('ChessScouterApp')
+    .factory('pins', function() {
+        return function(game, x, y) {
+            // return a list of pins for the piece located at (x, y) in the game
+        }
+    });
