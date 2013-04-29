b638582192319415b5a7e4d45f14433630a50295
CaptainStack <S_L_Andre@hotmail.com>
2013-04-28 19:27:38 -0700

    Manually added all pieces

--- Game.js
+++ Game.js
@@ -1,43 +1,6 @@
 "use strict";
 function Game() {    
     this.turn = 1;
-
-    // var temp = [    
-    // new Piece("black", "rook", 0, 0, false, false),
-    // new Piece("black", "knight", 1, 0, false, false),
-    // new Piece("black", "bishop", 2, 0, false, false),
-    // new Piece("black", "queen", 3, 0, false, false),
-    // new Piece("black", "king", 4, 0, false, false),
-    // new Piece("black", "bishop", 5, 0, false, false),
-    // new Piece("black", "knight", 6, 0, false, false),
-    // new Piece("black", "rook", 7, 0, false, false),
-    // new Piece("black", "pawn", 0, 1, false, false),
-    // new Piece("black", "pawn", 1, 1, false, false),
-    // new Piece("black", "pawn", 2, 1, false, false),
-    // new Piece("black", "pawn", 3, 1, false, false),
-    // new Piece("black", "pawn", 4, 1, false, false),
-    // new Piece("black", "pawn", 5, 1, false, false),
-    // new Piece("black", "pawn", 6, 1, false, false),
-    // new Piece("black", "pawn", 7, 1, false, false),
-
-    // new Piece("white", "rook", 0, 7, false, false),
-    // new Piece("white", "knight", 1, 7, false, false),
-    // new Piece("white", "bishop", 2, 7, false, false),
-    // new Piece("white", "queen", 3, 7, false, false),
-    // new Piece("white", "king", 4, 7, false, false),
-    // new Piece("white", "bishop", 5, 7, false, false),
-    // new Piece("white", "knight", 6, 7, false, false),
-    // new Piece("white", "rook", 7, 7, false, false),
-    
-    // new Piece("white", "pawn", 0, 6, false, false),
-    // new Piece("white", "pawn", 1, 6, false, false),
-    // new Piece("white", "pawn", 2, 6, false, false),
-    // new Piece("white", "pawn", 3, 6, false, false),
-    // new Piece("white", "pawn", 4, 6, false, false),
-    // new Piece("white", "pawn", 5, 6, false, false),
-    // new Piece("white", "pawn", 6, 6, false, false),
-    // new Piece("white", "pawn", 7, 6, false, false)
-    // ];
     
     var grid = [];
     for(var i = 0; i < 8; i++){
@@ -57,16 +20,33 @@ function Game() {
     grid[6][0] = new Square(6, 0, new Piece("black", "knight", false, false));
     grid[7][0] = new Square(7, 0, new Piece("black", "rook", false, false));
     
-    // grid[1][0] = new Square(0, 0, new Piece("black", "pawn", 0, 1, false, false));
-    // grid[1][1] = new Square(1, 0, new Piece("black", "pawn", 1, 1, false, false));
-    // grid[1][2] = new Square(2, 0, new Piece("black", "pawn", 2, 1, false, false));
-    // grid[1][3] = new Square(3, 0, new Piece("black", "pawn", 3, 1, false, false));
-    // grid[1][4] = new Square(4, 0, new Piece("black", "pawn", 4, 1, false, false));
-    // grid[1][5] = new Square(5, 0, new Piece("black", "pawn", 5, 1, false, false));
-    // grid[1][6] = new Square(6, 0, new Piece("black", "pawn", 6, 1, false, false));
-    // grid[1][7] = new Square(7, 0, new Piece("black", "pawn", 7, 1, false, false));
+    grid[0][1] = new Square(0, 1, new Piece("black", "pawn", false, false));
+    grid[1][1] = new Square(1, 1, new Piece("black", "pawn", false, false));
+    grid[2][1] = new Square(2, 1, new Piece("black", "pawn", false, false));
+    grid[3][1] = new Square(3, 1, new Piece("black", "pawn", false, false));
+    grid[4][1] = new Square(4, 1, new Piece("black", "pawn", false, false));
+    grid[5][1] = new Square(5, 1, new Piece("black", "pawn", false, false));
+    grid[6][1] = new Square(6, 1, new Piece("black", "pawn", false, false));
+    grid[7][1] = new Square(7, 1, new Piece("black", "pawn", false, false));
     
-    console.log(grid);
+    grid[0][6] = new Square(0, 6, new Piece("white", "pawn", false, false));
+    grid[1][6] = new Square(1, 6, new Piece("white", "pawn", false, false));
+    grid[2][6] = new Square(2, 6, new Piece("white", "pawn", false, false));
+    grid[3][6] = new Square(3, 6, new Piece("white", "pawn", false, false));
+    grid[4][6] = new Square(4, 6, new Piece("white", "pawn", false, false));
+    grid[5][6] = new Square(5, 6, new Piece("white", "pawn", false, false));
+    grid[6][6] = new Square(6, 6, new Piece("white", "pawn", false, false));
+    grid[7][6] = new Square(7, 6, new Piece("white", "pawn", false, false));
+    
+    grid[0][7] = new Square(0, 7, new Piece("white", "rook", false, false));
+    grid[1][7] = new Square(1, 7, new Piece("white", "knight", false, false));
+    grid[2][7] = new Square(2, 7, new Piece("white", "bishop", false, false));
+    grid[3][7] = new Square(3, 7, new Piece("white", "queen", false, false));
+    grid[4][7] = new Square(4, 7, new Piece("white", "king", false, false));
+    grid[5][7] = new Square(5, 7, new Piece("white", "bishop", false, false));
+    grid[6][7] = new Square(6, 7, new Piece("white", "knight", false, false));
+    grid[7][7] = new Square(7, 7, new Piece("white", "rook", false, false));
+
     this.gameBoard = new Board(grid);
 }
 Game.prototype.getTurn = function getTurn(){
