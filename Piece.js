9d8b2ba762a8ffab44aefbe03855468d7f28d9e4
CaptainStack <S_L_Andre@hotmail.com>
2013-04-28 18:08:36 -0700

    Removed IDs from pieces

--- Piece.js
+++ Piece.js
@@ -1,32 +1,14 @@
 "use strict";
-function Piece(id, color, type, x, y, captured, hasMoved) {    
+function Piece(color, type, captured, hasMoved) {    
     this.color = color;
     this.type = type;
-    this.x = x;
-    this.y = y;
     this.captured = captured;
     this.hasMoved = hasMoved;
-    this.id = id;
     this.image = color + "_" + type + ".svg";
-}
-Piece.prototype.movePiece = function movePiece(newX, newY) {
-    this.x = newX;
-    this.y = newY;
-    alert("you moved me! to " + this.x + " " + this.y);
-}
-Piece.prototype.getSymbol = function getSymbol(){
-    if(this.type.charAt(0) != "k"){
-        return this.type.charAt(0).toUpperCase();
-    }else{
-        return this.type.substring(0, 2).toUpperCase();
-    }
 }
 Piece.prototype.getImage = function getImage(){
     return this.image;
 }
 Piece.prototype.isCaptured = function isCaptured(){
     return this.captured;
-}
-Piece.prototype.getPosition = function getPosition(){
-    return {x : this.x, y : this.y};
 };
