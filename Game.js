e5f57f7cc228902586d059c9a0010bf9a681454d
CaptainStack <S_L_Andre@hotmail.com>
2013-05-06 14:07:45 -0700

    Added King class

--- Game.js
+++ Game.js
@@ -15,7 +15,7 @@ function Game() {
     grid[1][0] = new Square(1, 0, new Knight("black"));
     grid[2][0] = new Square(2, 0, new Piece("black", "bishop", false, false));
     grid[3][0] = new Square(3, 0, new Piece("black", "queen", false, false));
-    grid[4][0] = new Square(4, 0, new Piece("black", "king", false, false));
+    grid[4][0] = new Square(4, 0, new King("black"));
     grid[5][0] = new Square(5, 0, new Piece("black", "bishop", false, false));
     grid[6][0] = new Square(6, 0, new Knight("black"));
     grid[7][0] = new Square(7, 0, new Piece("black", "rook", false, false));
---
+++ King.js
@@ -0,0 +1,46 @@
+"use strict";
+function King(color) {
+    this.color = color;
+    this.captured = false;
+    this.image = color + "_king.svg";
+}
+King.prototype.getColor = function getColor(){
+    return this.color;
+}
+King.prototype.getImage = function getImage(){
+    return this.image;
+}
+King.prototype.isCaptured = function isCaptured(){
+    return this.captured;
+}
+King.prototype.setMoved = function setMoved(movedState){
+    this.hasMoved = movedState;
+}
+King.prototype.getPotentialMoves = function getPotentialMoves(x, y){
+
+}
+King.prototype.getLegalMoves = function getLegalMoves(currentPosition){
+    var x = currentPosition.x;
+    var y = currentPosition.y;
+    var potentialMoves = [
+                        new Position(x, y + 1), 
+                        new Position(x, y - 1),
+                        new Position(x + 1, y),
+                        new Position(x - 1, y),
+                        new Position(x + 1, y + 1),
+                        new Position(x + 1, y - 1),
+                        new Position(x - 1, y + 1),
+                        new Position(x - 1, y - 1)
+                    ];
+    for(var i = potentialMoves.length - 1; i >= 0; i--){
+        if(potentialMoves[i].x < 0 || 
+            potentialMoves[i].x > 7 || 
+            potentialMoves[i].y < 0 || 
+            potentialMoves[i].y > 7 || 
+            myGame.gameBoard.occupiedBy(potentialMoves[i].x, potentialMoves[i].y) == myGame.whoseTurn())
+        {
+            potentialMoves.splice(i, 1);
+        }
+    }
+    return potentialMoves;
+};
