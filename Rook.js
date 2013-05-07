1cece4f9a75e31daa2527eb305bde838ea16dc74
CaptainStack <S_L_Andre@hotmail.com>
2013-05-06 23:17:14 -0700

    Got Rook's getLegalMoves to work

--- Rook.js
+++ Rook.js
@@ -24,11 +24,15 @@ Rook.prototype.getPotentialMoves = function getPotentialMoves(x, y){
 	var vectorWest = false
 	var addX = 0
 	var addY = 0
+    var northMoves = [];
+    var southMoves = [];
+    var eastMoves = [];
+    var westMoves = [];
 	while(vectorNorth == false || vectorSouth == false || vectorEast == false || vectorWest == false){
 		if(vectorNorth == false) {
 			addY--;
 			if( myGame.gameBoard.isOnBoard(new Position(x + addX,y + addY))){
-				potentialMoves.push(new Position (x + addX, y + addY));
+				northMoves.push(new Position (x + addX, y + addY));
 			}else {
 				addY = 0;
 				vectorNorth = true;
@@ -36,32 +40,47 @@ Rook.prototype.getPotentialMoves = function getPotentialMoves(x, y){
 		}else if (vectorSouth == false) {
 			addY++;
 			if( myGame.gameBoard.isOnBoard(new Position(x + addX,y + addY))){
-				potentialMoves.push(new Position (x + addX, y + addY));
+				southMoves.push(new Position (x + addX, y + addY));
 			}else {
 				addY = 0;
 				vectorSouth = true;
 			}
 		}else if (vectorEast == false){
-			addX--;
+            addX--;
 			if( myGame.gameBoard.isOnBoard(new Position(x + addX,y + addY))){
-				potentialMoves.push(new Position (x + addX, y + addY));
+				eastMoves.push(new Position (x + addX, y + addY));
 			}else {
 				addX = 0;
 				vectorEast = true;
 			}
-		
 		}else if (vectorWest == false){
-			addX++;
+            addX++;
 			if( myGame.gameBoard.isOnBoard(new Position(x + addX,y + addY))){
-				potentialMoves.push(new Position (x + addX, y + addY));
+				westMoves.push(new Position (x + addX, y + addY));
 			}else {
 				addX = 0;
 				vectorWest = true;
 			}
 		}
-	}	
+	}
+    potentialMoves.push(northMoves);
+    potentialMoves.push(southMoves);
+    potentialMoves.push(eastMoves);
+    potentialMoves.push(westMoves);
 	return potentialMoves;
 }
 Rook.prototype.getLegalMoves = function getLegalMoves(currentPosition){
-    return this.getPotentialMoves(currentPosition.x, currentPosition.y);
+    var legalMoves = [];
+    var allMoves = this.getPotentialMoves(currentPosition.x, currentPosition.y)
+    for(var i = 0; i < allMoves.length; i++){
+        var currVectorMoves = allMoves[i];
+        for(var j = 0; j < currVectorMoves.length; j++){
+            if(myGame.gameBoard.occupiedBy(currVectorMoves[j]) == null || myGame.gameBoard.occupiedBy(currVectorMoves[j]) != myGame.whoseTurn()){
+                legalMoves.push(currVectorMoves[j]);
+            }else{
+                break;
+            }
+        }
+    }
+    return legalMoves;
 };
