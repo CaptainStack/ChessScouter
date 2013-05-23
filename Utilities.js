997ce7b4c428ab5cc87294c7f9c455e71cc1f85c
ashishsc <ashish.s.chandwani@gmail.com>
2013-05-22 22:27:23 -0700

    Added method method

--- Utilities.js
+++ Utilities.js
@@ -1,9 +1,21 @@
 // This file contains utility functions
 
+// Define an inheritance function (copy the prototype chain of one
+// function and give it to the other)
 if (typeof Object.beget !== 'function') {
-     Object.beget = function (o) {
-         var F = function () {};
-         F.prototype = o;
-         return new F();
+	Object.beget = function (o) {
+		var F = function () {};
+		F.prototype = o;
+		return new F();
 	}; 
 }
+
+
+// Add a method conditionally to the Function type.
+Function.prototype.method = function (name, func) {
+	if (!this.prototype[name]) {
+		this.prototype[name] = func;    
+	} 
+};
+
+
