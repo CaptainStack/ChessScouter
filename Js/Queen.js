256d43d51c90f7215ea7a0823b603c59fbabb4cc
pabrams <pabrams@justice.gc.ca>
2013-08-28 16:03:16 -0400

    Update Queen.js

--- Js/Queen.js
+++ Js/Queen.js
@@ -8,16 +8,16 @@ function Queen(color) {
     this.symbol = "Q";
 }
 
-Queen.prototype.getPosition = function getPosition() {
-    for (var i = 0; i < game.board.grid.length; i++) {
-        for (var j = 0; j < game.board.grid[i].length; j++) {
-            if (game.board.grid[j][i].piece === this) {
-                return new Position(j, i);
-            }
-        }
-    }
-    return new Position(this.x, this.y);
-}
+// Queen.prototype.getPosition = function getPosition() {
+    // for (var i = 0; i < game.board.grid.length; i++) {
+        // for (var j = 0; j < game.board.grid[i].length; j++) {
+            // if (game.board.grid[j][i].piece === this) {
+                // return new Position(j, i);
+            // }
+        // }
+    // }
+    // return new Position(this.x, this.y);
+// }
 
 Queen.prototype.getPotentialMoves = function getPotentialMoves(position) {
     var x = position.x;
@@ -167,14 +167,14 @@ Queen.prototype.getAttacks = function getAttacks(currentPosition) {
     }
     return legalMoves;
 }
-Queen.prototype.cloneSelf = function cloneSelf() {
-    var selfClone = new Queen(this.color);
-    selfClone.color = this.color;
-    selfClone.captured = this.captured;
-    selfClone.image = this.image;
-    selfClone.hasMoved = this.hasMoved;
-    selfClone.material = this.material;
-    selfClone.type = this.type;
-    selfClone.symbol = this.symbol;
-    return selfClone;
-};
+// Queen.prototype.cloneSelf = function cloneSelf() {
+    // var selfClone = new Queen(this.color);
+    // selfClone.color = this.color;
+    // selfClone.captured = this.captured;
+    // selfClone.image = this.image;
+    // selfClone.hasMoved = this.hasMoved;
+    // selfClone.material = this.material;
+    // selfClone.type = this.type;
+    // selfClone.symbol = this.symbol;
+    // return selfClone;
+// };
