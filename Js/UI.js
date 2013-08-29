0ac8ed8b3b28f6a139aec48fcdc9d2fd3cc55606
CaptainStack <S_L_Andre@hotmail.com>
2013-08-28 19:33:18 -0700

    Changed messaging method. Added more helptext.

--- Js/UI.js
+++ Js/UI.js
@@ -65,7 +65,8 @@ function downloadPgn () {
 
 function alertHelp() {
     var text = $(this).context.parentNode.textContent;
-    var option = text.substring(0, text.length - 4);
+    // var option = text.substring(0, text.length - 4);
+    var option = $(this).attr("id");
     $.getJSON("Js/Messages.json", function(json) {
         alert(json[option]);
     });
